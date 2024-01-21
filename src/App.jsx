ce648ffc2f13ce0959bb8ba1e10d68eb2a1a3784
hanae <hanae.manekoudi@gmail.com>
2024-01-21 15:16:45 +0100

    Implement Profie

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import "./App.css";
 import MyCatalog from "./Components/MyCatalog/MyCatalog";
+import MyCertificates from "./Components/MyCertificates/MyCertificates";
 import MyCourses from "./Components/MyCourses/MyCourses";
 import MyPlacementTest from "./Components/MyPLacementsTest/MyPlacementTest";
+import MyProfile from "./Components/MyProfile/MyProfile";
 import Layout from "./Layouts/Layout/Layout";
 import { Routes, Route } from "react-router-dom";
 
@@ -13,6 +15,8 @@ function App() {
           <Route path="my-courses" element={<MyCourses />} />
           <Route path="my-catalog" element={<MyCatalog />} />
           <Route path="my-placement-test" element={<MyPlacementTest />} />
+          <Route path="my-certificates" element={<MyCertificates />} />
+          <Route path="my-profile" element={<MyProfile />} />
         </Routes>
       </Layout>
     </section>
---
+++ src/Components/MyCertificates/MyCertificates.jsx
@@ -0,0 +1,12 @@
+
+
+const MyCertificates = () => {
+  return (
+    <div className="d-flex justify-content-between align-items-center border-bottom pt-5"style={{paddingRight:"15rem"}}>
+    <p className="text-secondary fs-4 fw-bold">Certificates name</p>
+    <p className="text-secondary fs-4 fw-bold">Action</p>
+    </div>
+  )
+}
+
+export default MyCertificates
